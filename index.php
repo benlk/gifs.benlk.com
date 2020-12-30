4ca630df136f6cd887a5a01174745925abbd6c99
Ben Keith <ben@benlk.com>
2020-12-29 23:08:20 -0500

    Hash out the structure of index.php

--- index.php
+++ index.php
@@ -5,5 +5,24 @@
  * This project:
  * 1. reads in a list of files from the present directory
  * 2. reads in sources.csv
- * 3. outputs a templated structure listing all files, with description information.
+ * 3. outputs a templated structure listing all files,
+ *    with description information.
+ *
+ * This is meant to exist as a single file that can be
+ * dropped into a directory with minimal additional work.
  */
+
+function get_files() {
+}
+
+function get_csv() {
+}
+
+function merge_list( $files, $csv ) {
+}
+
+function get_csv_headers( $csv ) {
+}
+
+function render_table( $merged_list ) {
+}

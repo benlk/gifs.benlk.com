47114b857b63a00c74aa2202d2e3c55f178a9819
Ben Keith <ben@benlk.com>
2020-12-29 22:59:54 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * gifs.benlk.com
+ *
+ * This project:
+ * 1. reads in a list of files from the present directory
+ * 2. reads in sources.csv
+ * 3. outputs a templated structure listing all files, with description information.
+ */
